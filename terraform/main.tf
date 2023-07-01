6accf3e2c83576a0723a4a6714c26a6f6ab39f40
Argens <argens1203@gmail.com>
2023-07-01 18:29:09 +0800

    feat: use provided ami + region

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-east-1"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-06098e58989d56f7d"
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
