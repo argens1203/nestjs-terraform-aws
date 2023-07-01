48d85159458fa7e4cf30d68964f8f4f9ce3f8ba0
Argens <argens1203@gmail.com>
2023-07-01 18:23:24 +0800

    feat: update ami to fit region

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-06098e58989d56f7d"
   instance_type = "t2.micro"
 
   tags = {
